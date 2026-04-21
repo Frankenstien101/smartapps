a4fba6c22e80f90e18615bc2e21308d508605c51
Frankenstien101 <frankpadios.bspi@gmail.com>
2026-04-21 13:26:13 +0800

    n

--- SIDJAN/index.php
+++ SIDJAN/index.php
@@ -50,7 +50,6 @@ body {
     margin-left: 250px;
 }
 
-
 .submenu {
     padding-left: 20px;
 }
@@ -60,54 +59,107 @@ body {
 
 <!-- Sidebar -->
 <div id="sidebar" class="p-3">
-<div class="text-center mb-3">
-    <img src="MainImg/logo.png" alt="Delivery Dash Logo" style="height: 120px; width: 120px;">
-</div>    <hr>
+    <div class="text-center mb-3"> <img src="MainImg/logo.png" alt="Delivery Dash Logo" style="height: 120px; width: 120px;"> </div>
+    <hr>
     <ul class="nav flex-column">
         <li class="nav-item">
             <a class="nav-link" href="#"><i class="fa fa-home"></i> Dashboard</a>
         </li>
 
+        <!-- SALES -->
         <li class="nav-item">
-            <a class="nav-link" data-bs-toggle="collapse" href="#menu1">
-                <i class="fa fa-list"></i> Transactions
+            <a class="nav-link" data-bs-toggle="collapse" href="#menuSales">
+                <i class="fa fa-cash-register"></i> Sales
             </a>
-            <div class="collapse submenu" id="menu1">
-                <a class="nav-link" href="#">Sales Transaction</a>
-                <a class="nav-link" href="#">Loan Application</a>
-                <a class="nav-link" href="#">Payments</a>
+            <div class="collapse submenu" id="menuSales">
+                <a class="nav-link" href="#">POS / New Sale</a>
+                <a class="nav-link" href="#">Sales Transactions</a>
+                <a class="nav-link" href="#">Receipts</a>
             </div>
         </li>
 
-
+        <!-- PRODUCTS -->
         <li class="nav-item">
-            <a class="nav-link" data-bs-toggle="collapse" href="#menu2">
+            <a class="nav-link" data-bs-toggle="collapse" href="#menuProducts">
+                <i class="fa fa-mobile-alt"></i> Devices & Accessories
+            </a>
+            <div class="collapse submenu" id="menuProducts">
+                <a class="nav-link" href="#">Mobile Phones</a>
+                <a class="nav-link" href="#">Accessories</a>
+                <a class="nav-link" href="#">Brands</a>
+                <a class="nav-link" href="#">Categories</a>
+            </div>
+        </li>
+
+        <!-- INVENTORY -->
+        <li class="nav-item">
+            <a class="nav-link" data-bs-toggle="collapse" href="#menuInventory">
                 <i class="fa fa-box"></i> Inventory
             </a>
-            <div class="collapse submenu" id="menu2">
-                <a class="nav-link" href="#">Products</a>
-                <a class="nav-link" href="#">Categories</a>
-                <a class="nav-link" href="#">Stock</a>
-            </div>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" data-bs-toggle="collapse" href="#menu3">
-                <i class="fa fa-users"></i> Users
-            </a>
-            <div class="collapse submenu" id="menu3">
-                <a class="nav-link" href="#">User List</a>
-                <a class="nav-link" href="#">Roles</a>
+            <div class="collapse submenu" id="menuInventory">
+                <a class="nav-link" href="#">Stock In</a>
+                <a class="nav-link" href="#">Stock Out</a>
+                <a class="nav-link" href="#">Stock Transfer</a>
+                <a class="nav-link" href="#">Inventory Count</a>
             </div>
         </li>
 
+        <!-- TRANSACTIONS -->
         <li class="nav-item">
-            <a class="nav-link" data-bs-toggle="collapse" href="#menu4">
+            <a class="nav-link" data-bs-toggle="collapse" href="#menuTransactions">
+                <i class="fa fa-receipt"></i> Transactions
+            </a>
+            <div class="collapse submenu" id="menuTransactions">
+                <a class="nav-link" href="#">Sales History</a>
+                <a class="nav-link" href="#">Returns / Warranty</a>
+                <a class="nav-link" href="#">Repairs</a>
+                <a class="nav-link" href="#">Installments</a>
+            </div>
+        </li>
+
+        <!-- SUPPLIERS -->
+        <li class="nav-item">
+            <a class="nav-link" data-bs-toggle="collapse" href="#menuSuppliers">
+                <i class="fa fa-truck"></i> Suppliers
+            </a>
+            <div class="collapse submenu" id="menuSuppliers">
+                <a class="nav-link" href="#">Supplier List</a>
+                <a class="nav-link" href="#">Purchase Orders</a>
+                <a class="nav-link" href="#">Deliveries</a>
+            </div>
+        </li>
+
+        <!-- USERS -->
+        <li class="nav-item">
+            <a class="nav-link" data-bs-toggle="collapse" href="#menuUsers">
+                <i class="fa fa-users"></i> Users
+            </a>
+            <div class="collapse submenu" id="menuUsers">
+                <a class="nav-link" href="#">User List</a>
+                <a class="nav-link" href="#">Roles & Permissions</a>
+            </div>
+        </li>
+
+        <!-- REPORTS -->
+        <li class="nav-item">
+            <a class="nav-link" data-bs-toggle="collapse" href="#menuReports">
+                <i class="fa fa-chart-bar"></i> Reports
+            </a>
+            <div class="collapse submenu" id="menuReports">
+                <a class="nav-link" href="#">Sales Report</a>
+                <a class="nav-link" href="#">Inventory Report</a>
+                <a class="nav-link" href="#">Profit Report</a>
+            </div>
+        </li>
+
+        <!-- SETTINGS -->
+        <li class="nav-item">
+            <a class="nav-link" data-bs-toggle="collapse" href="#menuSettings">
                 <i class="fa fa-cog"></i> Settings
             </a>
-            <div class="collapse submenu" id="menu4">
-                <a class="nav-link" href="#">General</a>
-                <a class="nav-link" href="#">Security</a>
-                <a class="nav-link" style="background-color: red;" href="#">Logout</a>
+            <div class="collapse submenu" id="menuSettings">
+                <a class="nav-link" href="#">General Settings</a>
+                <a class="nav-link" href="#">System Setup</a>
             </div>
         </li>
     </ul>
@@ -121,15 +173,28 @@ body {
         </button>
 
         <div class="ms-auto d-flex align-items-center">
-            <span class="me-3">Welcome, <?php echo $_SESSION['username'] ?? 'user'; ?></span>
-            <img src="/mainimg/user.png" class="rounded-circle" style="height: 30px;">
+            <span class="me-3">Welcome, <?php echo $_SESSION['username'] ?? 'Guest'; ?></span>
+            <img src="https://via.placeholder.com/35" class="rounded-circle">
         </div>
     </div>
 </nav>
 
 <!-- Content -->
 <div id="content">
-    <h2>Dashboard</h2>
+    <?php
+        $page = $_GET['page'] ?? 'dashboard.php';
+
+        // basic security: prevent directory traversal
+        $page = basename($page);
+
+        $file = "pages/" . $page;
+
+        if (file_exists($file)) {
+            include $file;
+        } else {
+            echo "<h3>Page not found</h3>";
+        }
+    ?>
 </div>
 
 <!-- Scripts -->
